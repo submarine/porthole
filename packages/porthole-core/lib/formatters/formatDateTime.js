925517cf16b232b721e6b825900cbfe908697240
Gavin Ballard <gavin@gavinballard.com>
2025-07-22 15:29:36 +1000

    Fetch user timezone

--- packages/porthole-core/lib/formatters/formatDateTime.js
+++ packages/porthole-core/lib/formatters/formatDateTime.js
@@ -3,9 +3,19 @@ import { DateTime } from 'luxon';
 export const DATE_MED_WITH_WEEKDAY = 'DATE_MED_WITH_WEEKDAY';
 export const TIME_SIMPLE = 'TIME_SIMPLE';
 
+export const DEFAULT_TIMEZONE = 'Australia/Melbourne'
+
 export const formatDateTime = (dateTime, format) => {
   if (!dateTime) return '';
 
-  let zonedDateTime = DateTime.fromISO(dateTime).setZone('Australia/Melbourne');
+  let zonedDateTime = DateTime.fromISO(dateTime).setZone(getUserTimezone());
   return zonedDateTime.toLocaleString(DateTime[format]);
+}
+
+const getUserTimezone = () => {
+  try {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone || DEFAULT_TIMEZONE
+  } catch {
+    return DEFAULT_TIMEZONE
+  }
 }
